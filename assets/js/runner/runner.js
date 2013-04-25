686c7e4f27a7d87b85e3bcd98907f1a6668d0a18
Davidson Fellipe <email@fellipe.com>
2013-04-25 03:08:17 -0300

    removing canvas

--- assets/js/runner/runner.js
+++ assets/js/runner/runner.js
@@ -5,11 +5,16 @@ window.onload = function() {
     el.addEventListener("click", function(){
 
         for(var i = 0; i < 24; i++) {
-            var canvas1 = new Canvas(document.getElementById('canvas' + i));
+            var canvas = document.getElementById('canvas' + i);
 
             var colorxyz = xyz_from_wavelength(COLOR[i]);
+            var colorRGB = corCIEXYZtosRGB(colorxyz);
 
-            canvas1.draw(corCIEXYZtosRGB(colorxyz));
+            // debugger;
+            canvas.style.backgroundColor = "rgba(" + 
+                                            Math.floor(colorRGB.R) + "," +
+                                            Math.floor(colorRGB.G) + "," +
+                                            Math.floor(colorRGB.B) + ", 1)";
         }
     }, false);
 };
